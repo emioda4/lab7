55699a6f8f4e5b87c1e8d226dfe50b1ccb70378e
Emi Oda <emoda@ucsd.edu>
2019-02-21 10:46:14 -0800

    finished

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,16 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
+//it's calling to projects.json and creating a new property
+//that gets added to the json file called viewAlt. which would
+//be equal to false
 exports.view = function(request, response){
+  	projects["viewAlt"] = false;
   	response.render('index', projects);
+};
+
+
+exports.viewAlt = function(req, res){
+	projects["viewAlt"] = true;
+  	res.render('index', projects);
 };
